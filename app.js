180a32f8472909aa148eb606d4ffa3a6ddeb87f6
Purva shrivastva <shpurva6@gmail.com>
2024-07-07 09:25:19 +0530

    fix: correct syntax and add necessary middleware
    - Fixed require statements
    - Corrected app.use for URL-encoded body parsing
    - Fixed static files directory path
    - Added server startup log

--- app.js
+++ app.js
@@ -1,48 +1,5 @@
 // const express = require("express");
 // const bodyParser = require("body-parser");
-// var app = express();
-// app.set("view engine", "ejs");
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.static("public"));
-
-// const mongoose = require("mongoose");
-// mongoose.connect("mongodb://localhost:27017/todo");
-// const trySchema = new mongoose.Schema({
-//   name: String,
-// });
-// const item = mongoose.model("task", trySchema);
-// const todo = new item({
-//   name: "Let's create some videos",
-// });
-// const todo1 = new item({
-//   name: "Let's learn DSA",
-// });
-// const todo2 = new item({
-//   name: "Let's chill",
-// });
-// const todo3 = new item({
-//   name: "Let's code",
-// });
-
-// // todo.save();
-// // todo1.save();
-// // todo2.save();
-// // todo3.save();
-// app.get("/", function (req, res) {
-//   item.find({}, function (err, foundItems) {
-//     if (err) {
-//       console.log(err);
-//     } else {
-//       res.render("list", { ejes: foundItems });
-//     }
-//   });
-// });
-// app.listen("3000", function () {
-//   console.log("server started at port 3000");
-// });
-
-// const express = require("express");
-// const bodyParser = require("body-parser");
 // const mongoose = require("mongoose");
 
 // const app = express();
@@ -85,21 +42,23 @@
 //   }
 // });
 
-// app.post("/", function (req, res) {
+// app.post("/", async (req, res) => {
 //   const itemName = req.body.ele1;
 //   const todo4 = new item({
 //     name: itemName,
 //   });
-//   todo4.save();
+//   await todo4.save();
 //   res.redirect("/");
 // });
-// app.post("/delete", function (req, res) {
+
+// app.post("/delete", async (req, res) => {
 //   const checkedItem = req.body.checkbox1;
-//   item.findByIdAndRemove(checkedItem, function (err) {
-//     if (!err) {
-//       res.redirect("/");
-//     }
-//   });
+//   try {
+//     await item.findByIdAndRemove(checkedItem);
+//     res.redirect("/");
+//   } catch (err) {
+//     console.log(err);
+//   }
 // });
 
 // app.listen("3000", function () {
@@ -108,67 +67,25 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
+const ejs = require("ejs");
 
 const app = express();
+
 app.set("view engine", "ejs");
-app.set("views", __dirname + "/views"); // Explicitly set the views directory
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todo");
-
-const trySchema = new mongoose.Schema({
-  name: String,
-});
-const item = mongoose.model("task", trySchema);
-
-const todo = new item({
-  name: "Let's create some videos",
-});
-const todo1 = new item({
-  name: "Let's learn DSA",
-});
-const todo2 = new item({
-  name: "Let's chill",
-});
-const todo3 = new item({
-  name: "Let's code",
+app.get("/", function (req, res) {
+  res.render("home");
 });
 
-// todo.save();
-// todo1.save();
-// todo2.save();
-// todo3.save();
-
-app.get("/", async (req, res) => {
-  try {
-    const foundItems = await item.find({});
-    res.render("list", { ejes: foundItems });
-  } catch (err) {
-    console.log(err);
-  }
+app.get("/login", function (req, res) {
+  res.render("login");
+});
+app.get("/register", function (req, res) {
+  res.render("register");
 });
 
-app.post("/", async (req, res) => {
-  const itemName = req.body.ele1;
-  const todo4 = new item({
-    name: itemName,
-  });
-  await todo4.save();
-  res.redirect("/");
-});
-
-app.post("/delete", async (req, res) => {
-  const checkedItem = req.body.checkbox1;
-  try {
-    await item.findByIdAndRemove(checkedItem);
-    res.redirect("/");
-  } catch (err) {
-    console.log(err);
-  }
-});
-
-app.listen("3000", function () {
-  console.log("server started at port 3000");
+app.listen(9000, function () {
+  console.log("Server started on port 9000");
 });
